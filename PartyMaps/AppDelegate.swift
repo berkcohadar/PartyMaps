9687947272afdc8a839fbe9141b53acdd88316f9
Berk Cohadar <berkcohadar1@gmail.com>
2021-10-25 19:22:30 -0400

    GoogleMaps integration.

--- PartyMaps/AppDelegate.swift
+++ PartyMaps/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import GoogleMaps
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let GOOGLE_TOKEN = "AIzaSyB7YWn-W_zxxvAhePZOq4U6HbG6_JplBaA";
+        GMSServices.provideAPIKey(GOOGLE_TOKEN)
         return true
     }
 
