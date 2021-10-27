cd61a84f43c592b55cc59e55503f0df736317f2e
Berk Cohadar <berkcohadar1@gmail.com>
2021-10-26 20:22:23 -0400

    Second ViewController added.

---
+++ PartyMaps/EventsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  EventsViewController.swift
+//  PartyMaps
+//
+//  Created by Berk Çohadar on 10/26/21.
+//
+
+import UIKit
+
+class EventsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
